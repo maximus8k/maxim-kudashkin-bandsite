5e8d45eec959c48f6667ceced30356e9eee8b678
maximus8k <maksim.kud@gmail.com>
2023-01-22 23:57:40 -0500

    Sprint-3

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,59 @@
+
+const link = "https://project-1-api.herokuapp.com/showdates";
+const apiKey = "?api_key=55ad76c1-462d-4a73-a270-efbab320dd6a";
+
+const tableBody = document.querySelector(".table__body");
+
+showTable();
+//Function to take data from API
+function showTable() {
+  const getShows = axios.get(link + apiKey);
+  getShows.then((res) => {
+    const showsArrey = res.data;
+    tableBody.innerHTML = "";
+    showsArrey.forEach((show) => {
+      const tableRow = document.createElement("div");
+      tableRow.classList.add("table__row");
+      tableBody.appendChild(tableRow);
+
+      const tableDate = document.createElement("span");
+      tableDate.classList.add("table__date");
+      tableDate.classList.add("field")
+      tableDate.setAttribute("data-label", "date");
+
+      tableDate.innerHTML = new Date(Number(show.date)).toDateString();
+      tableRow.appendChild(tableDate);
+
+      const tableVenue = document.createElement("span");
+      tableVenue.setAttribute("data-label", "venue");
+      tableVenue.classList.add("field")
+      tableVenue.innerHTML = show.place;
+      tableRow.appendChild(tableVenue);
+
+      const tableCity = document.createElement("span");
+      tableCity.setAttribute("data-label", "city");
+      tableCity.classList.add("field")
+      tableCity.innerHTML = show.location;
+      tableRow.appendChild(tableCity);
+
+      const btnField = document.createElement("span");
+      btnField.classList.add("field")
+      tableRow.appendChild(btnField);
+
+      const tableBtn = document.createElement("button");
+      tableBtn.classList.add("btn");
+      tableBtn.innerHTML = "BUY TICKETS";
+      btnField.appendChild(tableBtn);
+    });
+  });
+}
+
+//Function to highlight row
+tableBody.addEventListener("click", (event) => {
+  const selectedRow = event.target.parentElement;
+  const allRows = document.querySelectorAll(".table__row");
+  allRows.forEach((row)=>{
+    row.classList.remove("table__row--selected");
+  })
+  selectedRow.classList.add("table__row--selected");
+});
---
+++ scripts/index-page.js
@@ -0,0 +1,103 @@
+const link = "https://project-1-api.herokuapp.com/comments";
+const apiKey = "?api_key=55ad76c1-462d-4a73-a270-efbab320dd6a";
+
+//Function to converty comment data and receive HTML card
+function displayComment(comment){
+        const commentLi = document.createElement("li");
+      commentLi.classList.add("item");
+      commentsList.appendChild(commentLi);
+
+      const itemImg = document.createElement("img");
+      itemImg.classList.add("item__img");
+      itemImg.setAttribute("src", " ");
+      commentLi.appendChild(itemImg);
+
+      const itemSection = document.createElement("div");
+      itemSection.classList.add("item__section");
+      commentLi.appendChild(itemSection);
+
+      const itemHeader = document.createElement("div");
+      itemHeader.classList.add("item__title");
+      itemSection.appendChild(itemHeader);
+
+      const itemName = document.createElement("p");
+      itemName.classList.add("item__name");
+      itemName.innerHTML = comment.name;
+      itemHeader.appendChild(itemName);
+
+      const itemDate = document.createElement("p");
+      itemDate.classList.add("item__date");
+      itemDate.innerText = new Date(comment.timestamp).toLocaleDateString(
+        "en-US"
+      );
+      itemHeader.appendChild(itemDate);
+
+      const itemBody = document.createElement("div");
+      itemBody.classList.add("item__body");
+      itemSection.appendChild(itemBody);
+
+      const bodyCopy = document.createElement("p");
+      bodyCopy.innerText = comment.comment;
+      itemBody.appendChild(bodyCopy);
+
+      const itemFooter = document.createElement("div");
+      itemFooter.classList.add("item__footer");
+      itemSection.appendChild(itemFooter);
+}
+//Function to return comments
+function showCommentsList() {
+  const getComments = axios.get(link + apiKey);
+  getComments.then((res) => {
+    const commentsData = res.data;
+    const commentsArrey = commentsData.sort(
+      (a, b) => b.timestamp - a.timestamp
+    );
+    commentsList.innerHTML = "";
+    commentsArrey.forEach((comment) => {
+      displayComment(comment);
+
+    });
+  });
+}
+
+const form = document.querySelector(".form");
+const commentsList = document.querySelector(".comments__list");
+showCommentsList();
+
+//Add Comment
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const submittedName = event.target.formName;
+  const submittedComment = event.target.formComment;
+  if (formValidation(submittedName,submittedComment)) {
+    const postedComment = {
+      name: submittedName.value,
+      comment: submittedComment.value,
+    };
+    const addComment = axios.post(link + apiKey, postedComment);
+    addComment.then((res) => {
+      event.target.reset();
+      showCommentsList();
+    });
+  }
+});
+
+
+//Function to check valid characters
+function formValidation (nameField, commentField) {
+  const re = /^[a-zA-Z]/
+  if (!re.test(nameField.value)) {
+    nameField.focus();
+    nameField.value = "";
+    nameField.classList.add("form__input--error");
+    return false
+
+  } else if (!re.test(commentField.value)) {
+      commentField.focus();
+      commentField.value = ""
+      commentField.classList.add("form__input--error");
+      return false
+  } else {
+    return true
+  }
+}
